7af6b7914fd67dd97b4c3d5e2bfc96772739baa9
Anushek Prasal <anushekprasal@gmail.com>
2018-11-12 01:10:05 +0530

    base: Add QS header styles [1/3]
    Gives the user a choice between black, dark grey, light grey and accent based options.
    
    Based on the code of Fruity pebbles and QS tile picker.
    
    @Aston-Martinn | Forward port to Q
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4236,6 +4236,18 @@ public class StatusBar extends SystemUI implements DemoMode,
         ThemesUtils.stockNotificationStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
+    // Switches qs header style from stock to custom
+    public void updateQSHeaderStyle() {
+        int qsHeaderStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_HEADER_STYLE, 0, mLockscreenUserManager.getCurrentUserId());
+        ThemesUtils.updateQSHeaderStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), qsHeaderStyle);
+    }
+
+    // Unload all qs header styles back to stock
+    public void stockQSHeaderStyle() {
+        ThemesUtils.stockQSHeaderStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+    }
+
     private void updateDozingState() {
         Trace.traceCounter(Trace.TRACE_TAG_APP, "dozing", mDozing ? 1 : 0);
         Trace.beginSection("StatusBar#updateDozingState");
@@ -5453,6 +5465,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NOTIFICATION_STYLE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_HEADER_STYLE),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -5500,6 +5515,10 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.NOTIFICATION_STYLE))) {
                 stockNotificationStyle();
                 updateNotificationStyle();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_HEADER_STYLE))) {
+                stockQSHeaderStyle();
+                updateQSHeaderStyle();
             }
             update();
         }
