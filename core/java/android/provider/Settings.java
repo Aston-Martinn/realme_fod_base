f32e04e03b4eaba310b70e9d1c7d423fdd526886
00day0 <therandomuser11@gmail.com>
2019-11-12 20:27:21 +0100

    SystemUI: add toggle to hide privacy chip view in qs header and statusbar [1/2]
    Signed-off-by: 00day0 <therandomuser11@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7008,6 +7008,12 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Enable\Disable visibility of privacy_chip view in QS
+         * @hide
+         */
+        public static final String PRIVACY_CHIP_VIEW = "privacy_chip_view";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -38,6 +38,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
 import android.provider.Settings.Global;
+import android.provider.Settings;
 import android.service.notification.ZenModeConfig;
 import android.telecom.TelecomManager;
 import android.text.format.DateFormat;
@@ -755,6 +756,8 @@ public class PhoneStatusBarPolicy
     }
 
     private void updatePrivacyItems(List<PrivacyItem> items) {
+        final boolean chipVisibilityDisabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PRIVACY_CHIP_VIEW, 1) == 1;
         boolean showCamera = false;
         boolean showLocation = false;
         for (PrivacyItem item : items) {
@@ -785,6 +788,10 @@ public class PhoneStatusBarPolicy
                 case TYPE_LOCATION:
                     showLocation = true;
                     break;
+            }
+            if (chipVisibilityDisabled) {
+                showCamera = false;
+                showLocation = false;
             }
         }
 
